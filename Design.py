92ef102b11f5c3f637b35eb8abfc5127df586623
HaloAssassin973 <cokturqi55@mail.ru>
2019-09-23 21:01:42 +0300

    Gui

--- Design.py
+++ Design.py
@@ -1,5 +1,15 @@
 import sys
-import PyQt5 import QtGui
-app = QtGui.QApplication(sys.argv)
-window = QtGui.QWidget()
-window.show()
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Simple')
+    w.show()
+
+    sys.exit(app.exec_())
