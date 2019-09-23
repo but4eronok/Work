363f24bd55935600e5d3c9348cd585aee00dee32
HaloAssassin973 <cokturqi55@mail.ru>
2019-09-23 20:31:55 +0300

    Add working template

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from docxtpl import DocxTemplate
 
-doc = DocxTemplate("шаблон.docx")
-context = { 'director' : "И.И.Иванов"}
+doc = DocxTemplate("Laba_3.docx")
+context = {'номер_лабы': '2', 'тема': 'Шаблоны в питоне', 'группа': 'ИУ10-114', 'студент_1': 'Пупа',
+           'студент_2': 'Лупа', 'преподаватель': 'бухгалтерия'}
 doc.render(context)
-doc.save("шаблон-final.docx")
+doc.save("Laba_3-final.docx")
