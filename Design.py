abbf377850cddc15a784b22d380a775a6d5af581
Kirill <kirill8256@rambler.ru>
2019-09-24 18:30:11 +0300

    Update Design.py

--- Design.py
+++ Design.py
@@ -1,5 +1,27 @@
 import sys
-import PyQt5 import QtGui
-app = QtGui.QApplication(sys.argv)
-window = QtGui.QWidget()
-window.show()
+from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication
+from PyQt5.QtGui import QIcon
+class Example(QMainWindow):
+    
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):               
+        exitAction = QAction(QIcon('exit.png'), '&Exit', self)        
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
+        self.statusBar()
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+        
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Menubar')    
+        self.show()  
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
